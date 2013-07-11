8c3b039a694bd7a0108278fae2e8fe2f60246421
Charlotte Gore <conspiracygore@gmail.com>
2013-07-11 23:19:16 +0100

    add lp filter, more tweaks

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ window.$ = require('dollar');
 window.measure = require('measure');
 var tick = require('tick');
 
-var freqs = [1244.51, 1108.73, 932.33, 830.61, 740.00, 622.25, 554.37, 466.16, 415.30, 370.00, 311.13, 277.18, 233.08, 207.65, 185.00, 155.56, 138.59]
+var freqs = [1244.51, 1108.73, 932.33, 830.61, 740.00, 622.25, 554.37, 466.16, 415.30, 370.00, 311.13, 277.18, 233.08, 207.65, 185.00, 155.56, 138.59, 116.54, 103.83, 92.50]
 
 var ctx = new webkitAudioContext();
-var bpm = 0.4;
+var bpm = 0.5;
 
 var osc = ctx.createOscillator();
 osc.connect(ctx.destination);
@@ -39,7 +39,7 @@ var Monome = function(){
 
 		for(var j = 0; j < 16; j++){
 
-			var button = new NoteButton(cubeSize, i, j, freqs[j]);
+			var button = new NoteButton(cubeSize, i, j, freqs[j + 2]);
 
 			ul.append(button.getElement());
 
@@ -60,7 +60,7 @@ var Monome = function(){
 	var index = 0;
 	var currentCol = 15;
 
-	var nextTime = ctx.currentTime + 0.5;
+	var nextTime = ctx.currentTime + bpm;
 
 	
 
@@ -95,7 +95,7 @@ var Monome = function(){
 				}				
 			}
 
-			nextTime = nextTime + 0.5;
+			nextTime = nextTime + bpm;
 
 		}
 	})
@@ -153,13 +153,23 @@ NoteButton.prototype = {
 
 		var osc = ctx.createOscillator();
 		var gain = ctx.createGain();
-		osc.connect(gain);
+		var filter = ctx.createBiquadFilter();
+
+		filter.type = 3;
+		filter.frequency.value = 5000;
+		filter.Q.value = 0.5;
+		filter.gain.value = 0
+
+		filter.connect(gain);
+
+		osc.connect(filter);
 		gain.connect(mixer);
 		gain.gain.value = 0;
 		osc.frequency.value = this.freq;
 		osc.start(ctx.currentTime);
 		osc.stop(startTime + (bpm + 0.5) );
 		osc.type=osc.SINE;
+
 
 		var start = Math.floor(startTime - ctx.currentTime) * 1000;
 
@@ -170,8 +180,8 @@ NoteButton.prototype = {
 			gain.gain.value = o.value;
 
 		}
-		var tweenA = require('tween').Tweening({ value: 0 }).to({ value: 1/count }).using('ease-in-expo').duration(100).tick(cb);
-		var tweenB = require('tween').Tweening({ value: 1/count }).to({ value: 0 }).using('ease-out-expo').duration(100).tick(cb);
+		var tweenA = require('tween').Tweening({ value: 0 }).to({ value: 1/count }).using('linear').duration(bpm * 1000).tick(cb);
+		var tweenB = require('tween').Tweening({ value: 1/count }).to({ value: 0 }).using('linear').duration(bpm * 500).tick(cb);
 		
 		setTimeout(function(){
 
